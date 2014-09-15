c542900d4e748fc5c9a63fdc85e6e900920d7f3f
Serena Chan <serenagchan@gmail.com>
2014-09-14 20:24:30 -0400

    Updated

--- 02-greeter/Greeter.java
+++ 02-greeter/Greeter.java
@@ -2,9 +2,7 @@ import java.io.*;
 import java.util.*;
 
 public class Greeter {
-	public static void main(String[] args) {
-		public void greet() {
-			System.out.println("Hello World!");
-		}
+	public void greet() {
+		System.out.println("Hello World!");
 	}
 }
