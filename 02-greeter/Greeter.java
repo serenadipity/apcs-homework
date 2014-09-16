0de3cb134cb6d6946b16da203d8e8cf9590e2e5c
serenadipity <serenagchan@gmail.com>
2014-09-16 08:00:29 -0400

    Updated

--- 02-greeter/Greeter.java
+++ 02-greeter/Greeter.java
@@ -2,7 +2,22 @@ import java.io.*;
 import java.util.*;
 
 public class Greeter {
-	public void Greet() {
-		System.out.println("Hello World!");
-	}
+    /*
+      public <- can be used outside of the class
+      void <- this DOES NOT return anything
+              you would place a "return type"
+	      here if the method did return something
+      greet <- That's the name
+      () <- parameters go here
+      { } <- the actual code goes between these
+
+    */
+    public void greet() {
+	System.out.println("Hello world!");
+	ungreet();
+    }
+    
+    private void ungreet() {
+	System.out.println("I'm out.");
+    }
 }
