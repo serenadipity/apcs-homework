bae223bc6ea9abc9668cb462e16662099a92c532
serenadipity <serenagchan@gmail.com>
2014-09-20 12:57:22 -0400

    Updated

--- 03-strings/Driver.java
+++ 03-strings/Driver.java
@@ -1,6 +1,12 @@
 public class Driver {
     public static void main(String[] args){
 	String s="serena chan";
-	//
+	System.out.println("My name is: "+s);
+	int location = s.indexOf(" ");
+	System.out.println("The location of the space is: "+location);
+	String first = s.substring(0, location);
+	System.out.println("My first name is: "+first);
+	String last = s.substring(location + 1);
+	System.out.println("My last name is: "+last);
     }
 }
