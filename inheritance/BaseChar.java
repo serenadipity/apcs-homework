42c219cc74862c3dd983d565fb69033674eba117
serenadipity <serenagchan@gmail.com>
2014-10-08 08:34:37 -0400

    Updated

--- inheritance/BaseChar.java
+++ inheritance/BaseChar.java
@@ -2,6 +2,23 @@
 
 public class BaseChar {
     private int health=20;
+    private String name = "default name";
+
+    public String toString(){
+	return this.getName();
+    }
+
+    public void attack(BaseChar other) {
+	System.out.println(this.getName() + " is attacking "+ other.getName());
+    }
+
+    public void setName(String name) {
+	this.name = name;
+    }
+
+    public String getName() {
+	return name;
+    }
 
     public int getHealth() {
 	return health;
--- inheritance/Driver.java
+++ inheritance/Driver.java
@@ -3,11 +3,21 @@ public class Driver {
         BaseChar c = new BaseChar();
 	Warrior w = new Warrior();
 	Mage m = new Mage();
-	c.setHealth(150);
+       	c.setHealth(150);
+	w.setName("Groo");
+	m.setName("Mr. Sage");
+	
+	w.attack(m);
+	m.attack(w);
+	w.attack(w);
+
+	System.out.println(w.toString());
+	/*
 	System.out.println(c.getHealth());
 	System.out.println(w.getHealth());
 	System.out.println(m.getHealth());
 
 	System.out.println(m.getManna());
-    }
+	*/
+    }	     
 }
