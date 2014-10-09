75095b81d2a140b79a3fc6ae0af462e6a0ee62f4
serenadipity <serenagchan@gmail.com>
2014-10-08 22:07:27 -0400

    Updated

--- inheritance/BaseChar.java
+++ inheritance/BaseChar.java
@@ -1,25 +1,23 @@
-/*superclass of Warrior */
-
-public class BaseChar {
+/* superclass of Warrior and Mage */
+public class Basechar {
     private int health=20;
-    private String name = "default name";
+    private String name="default name";
 
     public String toString(){
 	return this.getName();
     }
-
-    public void attack(BaseChar other) {
-	System.out.println(this.getName() + " is attacking "+ other.getName());
+    public void attack(Basechar other){
+	//System.out.println(this.getName()+" is attacking "+other.getName());
+	// toString is automatically called when you put a class instance in a string
+	// context
+	System.out.println(this+" is attacking "+other);
     }
-
     public void setName(String name) {
 	this.name = name;
     }
-
-    public String getName() {
+    public String getName(){
 	return name;
     }
-
     public int getHealth() {
 	return health;
     }
