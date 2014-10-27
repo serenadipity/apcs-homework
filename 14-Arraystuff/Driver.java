1b4ddd4192edecc1743391e7d0f339339c68a84f
serenadipity <serenagchan@gmail.com>
2014-10-27 18:27:52 -0400

    Updated Files

---
+++ 14-Arraystuff/Driver.java
@@ -0,0 +1,8 @@
+public class Driver {
+    public static void main(String[] args) {
+	Arraystuff2 x = new Arraystuff2();
+	System.out.println(x);
+	System.out.println("\n" + "Index: " + x.find(88));
+	System.out.println("\n" + "The max value is: " + x.maxvalue());
+    }
+}
