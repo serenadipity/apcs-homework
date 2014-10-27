afe98863951522ade70c5a4b56c8cbdddd845617
serenadipity <serenagchan@gmail.com>
2014-10-27 08:30:35 -0400

    Updated

---
+++ classwork/Arraystuff.java
@@ -0,0 +1,39 @@
+import java.io.*;
+import java.util.*;
+
+import java.util.*;
+
+public class Arraystuff {
+    private int[] a;
+    private Random rnd;
+    /* by making a variable final we can't change
+       the value after the initial asssignment 
+    */
+    public final int final_example = 123;
+
+    public Arraystuff(int n){
+	rnd = new Random();
+	a = new int[n];
+	for (int i=0;i<a.length;i++){
+	    a[i] = 75+rnd.nextInt(76);
+	}
+    }
+    
+    public Arraystuff() {
+	this(100);
+    }
+
+    public String toString(){
+	String s="";
+	for (int i=0;i<a.length;i++){
+	    s=s+a[i]+", ";
+	}
+	return s;
+    }
+    public static void main(String[] args) {
+	Arraystuff as = new Arraystuff();
+	System.out.println(as);
+	
+	
+    }
+}
