73265c4f7a2aa3427a15715a604db18ffb4f63d7
serenadipity <serenagchan@gmail.com>
2014-10-27 08:41:26 -0400

    Updated

---
+++ classwork/Arraystuff2.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.util.*;
+
+public class Arraystuff2.java {
+    private int[] a;
+    
+    public static void main(String[] args) {
+	
+	public int find(int value) {
+	    for (int i=0; i<a.length; i++) {
+		if (value==a[i]) {
+		    final = i;
+		}
+		else {
+		    System.out.println("N/A");
+		}
+		return i;
+	    }
+	}
+
+	/*	public int maxVal() {
+	    //
+	}
+	*/
+    }
+}
