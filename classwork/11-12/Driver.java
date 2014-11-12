99892e2cae3486b359a30ef2cbeee9c6fb880f82
serenadipity <serenagchan@gmail.com>
2014-11-12 08:37:06 -0500

    New Classwork

---
+++ classwork/11-12/Driver.java
@@ -0,0 +1,28 @@
+import java.io.*;
+import java.util.*;
+public class Driver {
+    public static void main(String[] args) {
+	ArrayList<String> al;
+	al = new ArrayList<String>();
+	System.out.println(al);
+	al.add("hello");
+	System.out.println(al);
+	
+	al.add("world");
+	System.out.println(al);
+	
+	al.add("33");
+	System.out.println(al);
+	System.out.println(al.size());
+	System.out.println(al.get(1));
+
+	ArrayList<Integer> ai;
+	ai = new ArrayList<Integer>();
+	Random r = new Random();
+	for (int i=0; i<20;i++) {
+	    ai.add(r.nextInt(8));
+	}
+	System.out.println(ai);
+    
+    }
+}
