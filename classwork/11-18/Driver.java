da2a572237e9e122b39237e7604354b73d37c006
serenadipity <serenagchan@gmail.com>
2014-11-19 08:29:50 -0500

    Updated

--- classwork/11-18/Driver.java
+++ classwork/11-18/Driver.java
@@ -1,5 +1,13 @@
 public class Driver {
     public static void main(String[] args) {
-	WordSearch ws = new WordSearch();
+	WordSearch w = new WordSearch();
+	System.out.println(w);
+	w.addWordH("hello",3,5);
+	w.addWordV("yooo",4,8);
+	w.addWordSE("hey",6,9);
+	w.addWordNE("hiii",11,13);
+	w.addWordNW("hihi",17,15);
+	w.addWordSW("hola",14,8);
+	System.out.println(w);
     }
 }
--- classwork/11-18/WordSearch.java
+++ classwork/11-18/WordSearch.java
@@ -1,24 +1,83 @@
 import java.io.*;
 import java.util.*;
 
-public class WordSearch {
+public class WordSearch{
 
     private char[][] board;
 
-    public WordSearch(int r, int c) {
+    public WordSearch(int r, int c){
 	board = new char[r][c];
+	for (int i = 0; i < board.length; i++) {
+	    for (int j = 0; j < board[i].length; j++) {
+		board[i][j]='.';
+	    }
+	}
+				
     }
-    public WordSearch() {
-	WordSearch(30,20);
+    public WordSearch(){
+	this(20,30);
     }
 
     public String toString(){
 	String s = "";
-	for (int i=0; i<board.length; i++) {
-	    for (int j=0; j<board[i].length; j++) {
-		s = s+ board[i][j] + " ";
+	for (int i = 0; i < board.length; i++) {
+	    for (int j = 0; j < board[i].length; j++) {
+		s = s + board[i][j];
 	    }
 	    s = s + "\n";
+	}
+	return s;
+    }
+
+    public void addWordH(String w, int row, int col) {
+	int r = row, c = col;
+	for (int i=0; i<w.length();i++) {
+	    board[r][c] = w.charAt(i);
+	    c++;
+	}
+    }
+    
+    public void addWordV(String w, int row, int col) {
+	int r = row, c = col;
+	for (int i=0; i<w.length();i++) {
+	    board[c][r] = w.charAt(i);
+	    c++;
+	}
+    }
+    
+    public void addWordSE(String w, int row, int col) {
+	int r = row, c = col;
+	for (int i=0; i<w.length();i++) {
+	    board[r][c] = w.charAt(i);
+	    c++;
+	    r++;
+	}
+    }
+
+    public void addWordNE(String w, int row, int col) {
+	int r = row, c = col;
+	for (int i=0; i<w.length();i++) {
+	    board[r][c] = w.charAt(i);
+	    c++;
+	    r--;
+	}
+    }
+
+    public void addWordNW(String w, int row, int col) {
+	int r = row, c = col;
+	for (int i=0; i<w.length();i++) {
+	    board[r][c] = w.charAt(i);
+	    c--;
+	    r--;
+	}
+    }
+
+    public void addWordSW(String w, int row, int col) {
+	int r = row, c = col;
+	for (int i=0; i<w.length();i++) {
+	    board[r][c] = w.charAt(i);
+	    c--;
+	    r++;
 	}
     }
 }
