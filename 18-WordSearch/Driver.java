7e4d13577b13b83e77e728fc3ddabafa2b304329
serenadipity <serenagchan@gmail.com>
2014-11-24 19:09:19 -0500

    Updated

--- 18-WordSearch/Driver.java
+++ 18-WordSearch/Driver.java
@@ -1,7 +1,7 @@
 public class Driver {
 
     public static void main(String[] args) {
-	WordSearch W = new WordSearch(30,50,"words.txt",10);
+	WordSearch W = new WordSearch(30,50,"LameVillains.txt",10);
 	W.MakePuzzle();
 	System.out.println(W);
 
