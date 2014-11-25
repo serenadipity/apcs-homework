c70ca146ebb5fd53df847a80cb2106e57ce211eb
serenadipity <serenagchan@gmail.com>
2014-11-25 08:09:48 -0500

    Updates

--- classwork/11-18/Driver.java
+++ classwork/11-18/Driver.java
@@ -1,13 +1,11 @@
 public class Driver {
+
     public static void main(String[] args) {
-	WordSearch w = new WordSearch();
-	System.out.println(w);
-	w.addWordH("hello",3,5);
-	w.addWordV("yooo",4,8);
-	w.addWordSE("hey",6,9);
-	w.addWordNE("hiii",11,13);
-	w.addWordNW("hihi",17,15);
-	w.addWordSW("hola",14,8);
-	System.out.println(w);
+	WordSearch W = new WordSearch(30,50,"LameVillians.txt",24);
+	W.MakePuzzle();
+	System.out.println();
+	System.out.println(W.PrintPuzzle());
+
     }
+
 }
