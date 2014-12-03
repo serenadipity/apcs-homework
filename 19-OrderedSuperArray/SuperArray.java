1f3a6ba511ed22bb70b44906d227a3894125a0b8
Serena Chan <serenagchan@gmail.com>
2014-12-02 20:36:09 -0800

    Update SuperArray.java

--- 19-OrderedSuperArray/SuperArray.java
+++ 19-OrderedSuperArray/SuperArray.java
@@ -1,69 +1,106 @@
 public class SuperArray {
-
-    private String[] data;
-
-    public SuperArray() {
-	data = new String[20];
-	for (int i=0; i<data.length, i++) {
-	    data[i] = "";
-	}
+    public String[] data;
+    public SuperArray(){
+    	data = new String[10];
+    	for (int j=0;j<data.length;j++){
+    		data[j]="";
+    	}
     }
-
-    public SuperArray(int i) {
+    public SuperArray(int i){
 	data = new String[i];
-	for (int j=0; j<data.length; j++) {
-	    data[j]="";
-	}
+	for (int j=0;j<data.length;j++){
+    	data[j]="";
     }
-
-    public boolean add(String str) {
-	int i = 0;
-	while (data[i]!="") {
-	    i++;
-	}
-	
-	    
-		
-	grow();
-	int i = 0;
-	while (data[i]!="") {
-	    i++;
-	}
-	data[i] = str;
     }
-
-    public void add( int index, int i ) {
-	if (index > size() ) {
-	    endpt = index;
-	}
-	grow();
-	for (int x=data.length-1;x>index;x=x-1) {
-	    data[x] = data[x-1];
-	}
-	data[index] = i;
-	endpt = index + 1;
-    }
-
-    public int get( int index ) {
-	return data[index];
-    }
-
-    public int size() {
-	return data.length;
-    }
-
-    public void set( int index, int i ) {
-       	data[index] = i;
-    }
-
-    public void grow() {
-	if ( endpt >= data.length ) {
-	    int[] end = new int[data.length + (data.length/2)];
-	    for (int i=0;i<data.length;i=i+1) {
-		end[i] = data[i];
+    public boolean add(String i){
+    	if (data.length == size()){
+	    String[] s = new s[data.length + 5];
+	    for (int m=0;m<data.length;m++){
+		s[m] = data[m];
 	    }
-	    data = end;
-	}
+	    data = s;
+	    for(int j=0;n<data.length;j++){
+		if(data[j].compareTo("") == 0){
+		    data[j] = i;
+		    break;
+		}
+	    }
+    	}
+    	else{
+	    for(int k=0;k<data.length;k++){
+		if(data[k].compareTo("") == 0){
+		    data[k] = i;
+		    break;
+		}
+	    }
+    	}
+	return true;
     }
-
+    public void add(int index, String i){
+    	if (index > data.length - 1 || index < 0){
+	    throw new ArrayIndexOutOfBoundsException();
+    	}
+    	if (data.length>size()){
+	    if (data[index].compareTo("") == 0){
+		data[index] = i;
+	    }
+	    else{
+		for(int n=data.length-1;n>index;n--){
+		    data[n] = data[n-1];
+		}
+		data[index] = i;
+	    }
+    	}
+    	else{
+	    String[] s = new String[data.length + 5];
+	    for (int n=0;n<data.length;n++){
+		s[n] = data[n];
+	    }
+	    data = s;
+	    if (data[index].compareTo("") == 0){
+		data[index] = i;
+	    }
+	    else{
+		for(int n=data.length-1;n>index;n--){
+		    data[n] = data[n-1];
+		}
+		data[index] = i;
+	    }
+    	}
+    	
+    }
+    public int size(){
+    	int result = 0;
+    	for(int i=0;i<data.length;i++){
+	    if (data[i].compareTo("") < 0){
+		result = result + 1;
+	    }
+    	}
+    	return result;
+    }
+    public String get(int index){
+    	return data[index];
+    }
+    public String set(int index, String i){
+    	String result = data[index];
+    	data[index] = i;
+    	return result;
+    }
+    public String remove(int index){
+    	String result = data[index];
+    	data[index] = "";
+    	return result;
+    }
+    public String toString(){
+	String s = "";
+	for (int i=0;i<data.length;i++){
+	    s = s + "Element " + i + " is " + data[i] + "\n";
+	}
+	return s;
+    }
+    
+    public static void main(String[] args){
+	SuperArray as = new SuperArray(10);
+	System.out.println(as.toString());
+    }
 }
