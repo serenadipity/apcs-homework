545fbed54b6d811fb2d4600cf35a42bdad8e3a97
Serena Chan <serenagchan@gmail.com>
2014-12-02 20:39:10 -0800

    Still needs fixing :(

--- 19-OrderedSuperArray/OrderedSuperArray.java
+++ 19-OrderedSuperArray/OrderedSuperArray.java
@@ -1,13 +1,38 @@
-public class OrderedSuperArray extends SuperArray {
+import java.io.*;
+import java.util.*;
 
-    private String[] data;
-    private int nextBlank;
-    private int endpt;
-
-    public OrderedSuperArray( int i ) {
-	data = new String[i];
-	nextBlank = 0;
-	endpt = i;
+public class OrderedSuperArray extends SuperArray{
+	public String[] data;
+    public static void main(String[] args){
+	OrderedSuperArray osa = new OrderedSuperArray(10);
+	osa.add("hi");
+	osa.add("heythere");
+	osa.add("hola");
+	osa.add("bonjour");
+	osa.add("ciao");
+	System.out.println(osa.toString());
     }
-    
+    public OrderedSuperArray(int i){
+	data = new String[i];
+	for (int j=0;j<data.length;j++){
+    	data[j]="";
+    }
+    }
+    public void add(int index, String i){
+	int length = i.length();
+	int j = 0;
+	while (length <= super.get(j).length()){
+	    j++;
+	}
+	super.add(j, i);
+    }
+    public boolean add(String i){
+	int length = i.length();
+	int j = 0;
+	while (length <= super.get(j).length()){
+	    j++;
+	}
+	super.add(j, i);
+	return true;
+    }
 }
