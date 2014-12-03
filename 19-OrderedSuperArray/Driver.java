67893e3c798a4b666e6a30d85439254b3bcbd8b1
Serena Chan <serenagchan@gmail.com>
2014-12-02 20:40:04 -0800

    Still needs fixing :(

--- 19-OrderedSuperArray/Driver.java
+++ 19-OrderedSuperArray/Driver.java
@@ -2,19 +2,6 @@ public class Driver {
 
     public static void main(String[] args) {
 	OrderedSuperArray A = new OrderedSuperArray(10);
-	A.add(1);
-	A.add(2);
-	A.add(3);
-	A.add(4);
-	A.add(5);
-	A.add(6);
-	A.add(7);
-	A.add(8);
-	A.add(9);
-	A.add(10);
-	A.add(11);
-	A.add(18,18);
-	A.set(3,33);
 	System.out.println(A);
 	int a = A.size();
 	System.out.println(a);
